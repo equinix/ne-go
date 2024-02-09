1800684e546e60baf08effca41cc95a49ada1003
kpdhulipala <kdhulipala@equinix.com>
2024-02-09 12:09:27 -0800

    project id filter on get accounts

--- internal/api/account.go
+++ internal/api/account.go
@@ -1,15 +1,16 @@
 package api
 
-//AccountResponse describes response to Network Edge
-//billing account query
+// AccountResponse describes response to Network Edge
+// billing account query
 type AccountResponse struct {
 	Accounts []Account `json:"accounts,omitempty"`
 }
 
-//Account describes Network Edge billing account
+// Account describes Network Edge billing account
 type Account struct {
-	Name   *string `json:"accountName,omitempty"`
-	Number *string `json:"accountNumber,omitempty"`
-	UCMID  *string `json:"accountUcmId,omitempty"`
-	Status *string `json:"accountStatus,omitempty"`
+	Name      *string `json:"accountName,omitempty"`
+	Number    *string `json:"accountNumber,omitempty"`
+	UCMID     *string `json:"accountUcmId,omitempty"`
+	Status    *string `json:"accountStatus,omitempty"`
+	ProjectID *string `json:"projectId,omitempty"`
 }
